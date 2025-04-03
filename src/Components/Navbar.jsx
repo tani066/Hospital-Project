9c348e8bc544a491ff2bc785cd0f14d92dcce268
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-04-03 17:22:23 +0530

    ui part completes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -54,10 +54,10 @@ const Navbar = () => {
                     <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-                    <NavLink className='px-4 py-2 rounded inline-block ' onClick={() => setShowMenu(false)} to='/'>Home</NavLink>
-                    <NavLink className='px-4 py-2 rounded inline-block ' onClick={() => setShowMenu(false)} to='/doctors'>All Doctors</NavLink>
-                    <NavLink className='px-4 py-2 rounded inline-block ' onClick={() => setShowMenu(false)} to='/about'>About</NavLink>
-                    <NavLink className='px-4 py-2 rounded inline-block ' onClick={() => setShowMenu(false)} to='/contact'>Contact</NavLink>
+                    <NavLink onClick={() => setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block '>Home</p></NavLink>
+                    <NavLink onClick={() => setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block '>All Doctors</p></NavLink>
+                    <NavLink onClick={() => setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block '> About</p></NavLink>
+                    <NavLink onClick={() => setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block '>Contact</p></NavLink>
                 </ul>
             </div>
         </div>
