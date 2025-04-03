e99735a9498ed38e0af1e08e6a18e27fd4f5fa03
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-04-03 15:24:09 +0530

    appointment page completed

--- src/Pages/MyAppointments.jsx
+++ src/Pages/MyAppointments.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../Context/Context'
+
 
 const MyAppointments = () => {
+  const {doctors} = useContext(Context)
   return (
     <div>
+      <p>My appointments</p>
+      <div>
+        {doctors.slice(0,3).map((item,index) => (
+          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key = {index}>
+            <div>
+              <img className='w-32 bg-indigo-50' src={item.image} alt="" />
+            </div>
+            <div className='flex-1 text-sm text-zinc-600'>
+              <p className='text-neutral-800 font-semibold'>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p className='text-zinc-700 font-medium mt-1'>Address:</p>
+              <p className='text-xs'>{item.address.line1}</p>
+              <p className='text-xs'>{item.address.line2}</p>
+              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> 01, April , 2025 | 8:30 PM</p>
+            </div>
+            <div></div>
+            <div className='flex flex-col gap-2 justify-end'>
+              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-blue-500 hover:text-white transition-all duration-300'>Pay Online</button>
+              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Cancel appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
       
     </div>
   )
