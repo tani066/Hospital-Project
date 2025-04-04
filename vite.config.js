96a7d40cb09249547fc60fc0edfa595f63f1901c
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-04-04 20:02:02 +0530

    changes in congig files

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ],
-  theme:{
-    extend:{
-      colors:{
-        'primary':'#5f6fff',
-      },
-      gridTemplateColumns:{
-        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
-      },
-    }
-  }
+  ]
 })
